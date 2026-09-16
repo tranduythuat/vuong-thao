2b28025566385a9168fa4cd72f4668728cd84872
tranduythuat <thuattran1810@gmail.com>
2026-09-16 17:59:14 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,6 +108,10 @@
 
     audio.addEventListener("play", () => icon.classList.add("spin"));
     audio.addEventListener("pause", () => icon.classList.remove("spin"));
+
+    if (!audio.paused) {
+      icon.classList.add("spin");
+    }
   }
 
   async function loadGuest() {
